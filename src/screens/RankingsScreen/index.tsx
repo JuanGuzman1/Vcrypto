7bfb242b054283ecd043c6dcaf9abe196c08217c
Juan Guzman <juanltd07@gmail.com>
2021-04-29 16:57:44 -0500

    ranking UI

--- src/screens/RankingsScreen/index.tsx
+++ src/screens/RankingsScreen/index.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import MarketCoin from "../../components/MarketCoin";
+import UserRankingItem from "../../components/UserRankingItem";
+import styles from "./styles";
+const image = require("../../../assets/images/Saly-20.png");
+
+const portfolioCoins = [
+  {
+    id: "1",
+    name: "Virtual Dollars",
+    image: "http://abancainnova.com/wp-content/uploads/2017/09/Bitcoin.png",
+    netWorth: 6942,
+  },
+  {
+    id: "2",
+    name: "Bitcoin",
+    image: "abc",
+    netWorth: 6942,
+  },
+  {
+    id: "3",
+    name: "Etherium",
+    image: "abc",
+    netWorth: 6942,
+  },
+];
 
 const RankingsScreen = () => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>
-                hello
-            </Text>
-        </View>
-    );
-}
+  return (
+    <View style={styles.root}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={portfolioCoins}
+        renderItem={({ item, index }) => <UserRankingItem user={item} place={index+1}/>}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponentStyle={{ alignItems: "center" }}
+        ListHeaderComponent={() => (
+          <>
+            <Image style={styles.image} source={image} />
+          </>
+        )}
+      />
+    </View>
+  );
+};
 
 export default RankingsScreen;
---
+++ src/screens/RankingsScreen/styles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    root:{
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: 'white'
+    },
+    image:{
+        height: 175,
+        resizeMode: 'contain',
+    },
+});
+
+export default styles;
