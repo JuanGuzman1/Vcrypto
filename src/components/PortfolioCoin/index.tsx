fee09838b883e0469d1d8ca5bbab15c41116e36e
Juan Guzman <juanltd07@gmail.com>
2021-05-23 16:31:55 -0500

    fetch portfolio data

--- src/components/PortfolioCoin/index.tsx
+++ src/components/PortfolioCoin/index.tsx
@@ -5,25 +5,30 @@ import styles from "./styles";
 
 export interface PortfoiloCoinProps {
   portfolioCoin: {
-    id: string
-    image: string;
-    name: string;
-    symbol: string;
     amount: number;
-    valueUSD: number;
+    coin: {
+      id: string;
+      image: string;
+      name: string;
+      symbol: string;
+      currentPrice: number;
+    };
   };
 }
 
 const PortfolioCoin = (props: PortfoiloCoinProps) => {
   const {
-    portfolioCoin: { id, image, name, symbol, amount, valueUSD },
+    portfolioCoin: {
+      amount,
+      coin: { id, image, name, symbol, currentPrice },
+    },
   } = props;
   const navigation = useNavigation();
 
   return (
     <Pressable
       style={styles.root}
-      onPress={() => navigation.navigate("CoinDetails", {id})}
+      onPress={() => navigation.navigate("CoinDetails", { id })}
     >
       <View style={styles.left}>
         <Image style={styles.image} source={{ uri: image }} />
@@ -33,7 +38,7 @@ const PortfolioCoin = (props: PortfoiloCoinProps) => {
         </View>
       </View>
       <View style={{ alignItems: "flex-end" }}>
-        <Text style={styles.value}>${valueUSD}</Text>
+        <Text style={styles.value}>${amount * currentPrice}</Text>
         <Text style={styles.symbol}>
           {symbol} {amount}
         </Text>
--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View, Text, Image } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-import {API, graphqlOperation} from 'aws-amplify';
-import {} from './queries';
+import { API, graphqlOperation } from "aws-amplify";
+import { getUserPortfolio } from "./queries";
 import PortfolioCoin from "../../components/PortfolioCoin";
 import styles from "./styles";
+import AppContext from "../../utils/AppContext";
 const image = require("../../../assets/images/Saly-10.png");
 
 const PortfolioScreen = () => {
+  const [balance, setBalance] = useState(0);
+  const [portfolioCoins, setPortfolioCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { userId } = useContext(AppContext);
+
+  const fetchPortfolio = async () => {
+    setLoading(true);
+    try {
+      const response = await API.graphql(
+        graphqlOperation(getUserPortfolio, { id: userId })
+      );
+      setBalance(response.data.getUser.networth)
+      setPortfolioCoins(response.data.getUser.portfolioCoins.items)
+    } catch (e) {
+      console.error(e);
+    }finally{
+      setLoading(false);
+    }
+  };
+
+  useEffect(()=>{
+    fetchPortfolio();
+  },[]);
+
   return (
     <View style={styles.root}>
       <FlatList
         style={{ width: "100%" }}
         data={portfolioCoins}
         renderItem={({ item }) => <PortfolioCoin portfolioCoin={item} />}
+        refreshing={loading}
+        onRefresh={fetchPortfolio}
         showsVerticalScrollIndicator={false}
-        ListHeaderComponentStyle={{alignItems: 'center'}}
+        ListHeaderComponentStyle={{ alignItems: "center" }}
         ListHeaderComponent={() => (
           <>
             <Image style={styles.image} source={image} />
             <View style={styles.balanceContainer}>
               <Text style={styles.label}>Portfolio balance</Text>
-              <Text style={styles.balance}>$69.420</Text>
+              <Text style={styles.balance}>${balance}</Text>
             </View>
           </>
         )}
